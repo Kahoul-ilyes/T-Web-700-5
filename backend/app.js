974f0e6818ac5ea1178bc1744b7cc5d957bf2f45
mdufour <mathieu.dufour@whereyoulove.com>
2020-01-20 15:11:08 +0100

    first try socketio

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ let path = require('path')
 let cookieParser = require('cookie-parser')
 let logger = require('morgan')
 
+
 let mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/cryptocodex', {useNewUrlParser: true, useUnifiedTopology: true})
 
@@ -33,5 +34,87 @@ app.use('/api/v0/auth', authRouter)
 app.use('/api/v0/users', usersRouter)
 app.use('/api/v0/cryptos', cryptosRouter)
 app.use('/api/v0/coins', coinsRouter)
+
+
+
+/**
+ * Socket IO
+ */
+const io = require('socket.io-client');
+const socket = io('wss://stream.binance.com:9443', {
+  path: '/ws/btcusdt@ticker'
+})
+
+socket.on('connect', () => {
+  console.log('socket connected : ')
+  console.log(socket.id); // 'G5p5...'
+})
+
+socket.on('connect_error', (error) => {
+  console.log('socket connect error : ')
+  throw error
+})
+
+socket.on('connect_timeout', (timeout) => {
+  console.log('socket timeout : ')
+  throw timeout
+})
+
+socket.on('error', (error) => {
+  console.log('socket error : ')
+  throw error
+})
+
+socket.on('disconnect', (reason) => {
+  console.log('socket disconnected : ')
+  if (reason === 'io server disconnect') {
+    // the disconnection was initiated by the server, you need to reconnect manually
+    socket.connect()
+  }
+  // else the socket will automatically try to reconnect
+})
+
+socket.on('ping', () => {
+  console.log('socket ping received : ')
+  socket.emit('pong')
+})
+
+
+/**
+ * Websocket
+ */
+// let WebSocket = require('ws')
+
+// let socket = null
+// try {
+//   // Connexion vers un serveur HTTPS
+//   // prennant en charge le protocole WebSocket over SSL ("wss://").
+//   socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@ticker")
+// } catch (exception) {
+//     console.error(exception)
+// }
+
+// // Récupération des erreurs.
+// // Si la connexion ne s'établie pas,
+// // l'erreur sera émise ici.
+// socket.onerror = (error) => {
+//     throw error
+// };
+
+// // Lorsque la connexion est établie.
+// socket.onopen = (event) => {
+//   console.log("Connected to binance websocket")
+
+//   // Lorsque la connexion se termine.
+//   this.onclose = (event) => {
+//     console.log("Disconnected")
+//   };
+
+//   // Lorsque le serveur envoi un message.
+//   this.onmessage = (event) => {
+
+//     console.log("Data :", event.data)
+//   };
+// };
 
 module.exports = app
