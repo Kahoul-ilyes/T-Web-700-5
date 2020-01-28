1009ec5b46f0b279711cf3287f0c3e84e8d0cb37
Administrator <leo.hamon@epitech.eu>
2020-01-28 16:34:31 +0100

    Auth0

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {CryptoListComponent} from './crypto/cryptolist/crypto-list.component';
 
 
+
 const routes: Routes = [
   { path: '', component: CryptoListComponent},
 
