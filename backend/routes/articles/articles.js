50a92d21b300b19903e4989cb8a9de74d3fe289b
pipchild <pipchild@free.fr>
2020-02-10 12:05:49 +0100

    Article route by keywords, err msg fixed

--- backend/routes/articles/articles.js
+++ backend/routes/articles/articles.js
@@ -115,7 +115,7 @@ router.get('/rss/', function (req, res) {
 // get user interest keywords, scan rss flux, send back to front concerned articles or every article if no keywords
 /*GET /articles[?params1=value1&...]
 params: free. User MUST be logged in (OR NOT). If the user is anonymous the settings (if any) are
-ignored and the last published articles are returned. If the user is logged in the settings are used to
+ignored and the last published articles are returned. If the user isuser found with this id logged in the settings are used to
 return only the items most relevant to the user (a list of keywords might help you). You are free to define
 the parameters that you think will be useful depending on the search options you offer to your users.
 Here for each article, you must provide at least:
@@ -216,7 +216,7 @@ router.get('/', function (req, res) {
     }
   } else {
 
-    console.log('No user found with this id, default: no keywords')
+    console.log('No keywords, default: no keywords')
     Article.find({}, function (err, result) {
       if (err) res.json({
         err: err
